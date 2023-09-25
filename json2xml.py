f85af6b22b2d19c2c3231a744377c06b415dc2d7
Lydia Körber <lydiaekoerber@gmail.com>
2023-09-25 14:13:45 +0200

    restructure to make TEI-validation, work with pre-filtered files
    * filtered Dateien nach Sebastians Skript genutzt (Bots ignoriert, gelöschte Kommentare entfernt)
    * Zeilenumbrüche in `lb`-Elemente umgewandelt
    * Subreddit-Namen im `title` gespeichert
    * bei den Kommentaren Author und Datum in `name`- und `date`-Subelementen hinzugefügt
    * Repräsentation der Kommentare umgestellt: `list` mit `item`-Elementen je Subkommenta

--- json2xml.py
+++ json2xml.py
@@ -1,19 +1,112 @@
 from datetime import datetime
 import json
+import lzma
 import os
+from pickle import load as load_pickle
+import traceback
 
-from lxml.etree import Element, ElementTree, SubElement, fromstring, tostring
+import lxml
+from lxml import etree
+from lxml.etree import (
+    Element,
+    ElementTree,
+    QName,
+    RelaxNG,
+    SubElement,
+    fromstring,
+    tostring
+)
+from trafilatura.xml import validate_tei
 
 
-def build_subcomments(supercomment_element, subcomment):
-    comment = SubElement(supercomment_element, 'comment', id=subcomment['id'], author=subcomment['author'])
-    comment.text = subcomment['body']
+TEI_SCHEMA = '../tei-schema-pickle.lzma'
+blacklist_authors = []
+
+
+def build_subcomments(supercomment_element, subcomment, filtered=True):
+    """
+    Build TEI XML subcomments within a parent comment element.
+
+    Args:
+        supercomment_element (Element): The parent comment element to which
+            subcomments will be added.
+        subcomment (dict): The subcomment data in a dictionary format.
+        filtered (bool, optional): Whether deleted comments and authors are
+            already removed. Defaults to True.
+
+    Description:
+        This function appends subcomments to a specified parent comment element
+        in TEI XML format.
+        Each subcomment is represented as a <p> element with attributes for
+        'id' and 'author'.
+        Additionally, line breaks within subcomment text are transformed into
+        <lb> elements.
+
+    """
+    if not subcomment['author'] in blacklist_authors:
+        comment = SubElement(supercomment_element, 'item',
+                             id=subcomment['id'])
+        # transform 'id' to 'xml:id' attribute
+        # https://github.com/knit-bee/tei-transform/blob/60ac079c0d91a9196e98c2be89c9c287cecee824/tei_transform/element_transformation.py#L18
+        namespace = "http://www.w3.org/XML/1998/namespace"
+        old_attr = 'id'
+        old_attribute = comment.attrib.get(old_attr)
+        if old_attribute is not None:
+            attr_value = comment.attrib.pop(old_attr)
+            new_attribute = etree.QName(namespace, old_attr)
+            comment.set(new_attribute, attr_value)
+        # author and date as subelements
+        author = SubElement(comment, 'name')
+        author.text = subcomment['author']
+        time = datetime.utcfromtimestamp(int(subcomment['created_utc']))
+        date = SubElement(comment, 'date')
+        date.text = str(time.date())
+        comment.text = subcomment['body']
+        # tranform line breaks to <lb>
+        if '\n' in subcomment['body']:
+            num_lb = subcomment['body'].count('\n')
+            # text until first line break
+            comment.text = subcomment['body'].split('\n')[0]
+            for i in range(num_lb):
+                line_break = Element("lb")
+                # text after line break
+                line_break.tail = subcomment['body'].split('\n')[i + 1]
+                comment.append(line_break)
+        else:
+            comment.text = subcomment['body']
+        # display date and author after text
+        comment.append(date)
+        comment.append(author)
     if subcomment['responses']:
+        comment_list = SubElement(comment, 'list')
         for r in subcomment['responses']:
-            if r['body'] != '[deleted]' and r['author'] != '[deleted]':
-                build_subcomments(comment, r)
+            if not filtered:
+                if r['body'] == '[deleted]' or r['author'] == '[deleted]':
+                    continue
+            build_subcomments(comment_list, r)
 
-def json2xml(file, tree_structure=True, output_dir='wohnen_xml'):
+
+def json2xml(file, tree_structure=True, output_dir='wohnen_xml', validate=False,
+             filtered=True, subreddit_loc='title'):
+    """
+    Convert Reddit JSON data to TEI XML format.
+
+    Args:
+        file (str): The path to the input JSON file.
+        tree_structure (bool, optional): Whether the input file is in a tree
+            structure. Defaults to True.
+        output_dir (str, optional): The directory to save the generated XML
+            file. Defaults to 'wohnen_xml'.
+        validate (bool, optional): Whether to validate the generated TEI XML.
+            Defaults to True.
+        filtered (bool, optional): Whether deleted comments and authors are
+            already removed. Defaults to True.
+        subreddit_loc (str, optional): Location to include subreddit
+            information ('profiledesc' or 'title'). Defaults to 'title'.
+
+    Returns:
+        str: The TEI XML document as a string.
+    """
     # read json file
     with open(file, 'r', encoding='latin-1') as f:
         txt = f.read()
@@ -23,61 +116,98 @@ def json2xml(file, tree_structure=True, output_dir='wohnen_xml'):
     # use first comment entry to extract meta data information
     _, info = list(comments.items())[0]
     time = datetime.utcfromtimestamp(int(info['created_utc']))
-    # FRAGE allgemeine Metadaten werden hier aus erstem comment extrahiert
-    # woher können wir das Datum vom Ursprungspost ziehen? genauso author?
     docmeta["date"] = str(time.date())
     # permalink example: /r/wohnen/comments/qqro38/wie_soll_ich_mit_meinen_überempfindlichen/hk1xuv3/
-    _, _, subreddit, _, post_id, title, comment_id, _ = info['permalink'].split('/')
-    # FRAGE _ ersetzen? casing rekonstruieren?
+    _, _, subreddit, _, post_id, title, comment_id, _ = \
+        info['permalink'].split('/')
+    # reconstruct title and URL from permalink
     docmeta["title"] = title
     docmeta["url"] = f'https://www.reddit.com/r/{subreddit}/comments/{post_id}/{title}/'
     docmeta["subreddit"] = subreddit
+    if subreddit_loc == 'title':
+        docmeta["title"] = f'{subreddit}/{title}'
 
     # build tei xml doc
     teidoc = Element("TEI", xmlns="http://www.tei-c.org/ns/1.0")
     header = SubElement(teidoc, 'teiHeader')
     filedesc = SubElement(header, 'fileDesc')
-    sourcedesc = SubElement(filedesc, 'sourceDesc')
-    source_bibl = SubElement(sourcedesc, 'bibl')
 
-    # FRAGE wo subreddit?
+    # title statement
     bib_titlestmt = SubElement(filedesc, 'titleStmt')
     bib_titlemain = SubElement(bib_titlestmt, 'title', type='main')
     bib_titlemain.text = docmeta["title"]
     if 'author' in docmeta.keys():
         bib_author = SubElement(bib_titlestmt, 'author')
         bib_author.text = docmeta.author
+    publicationstmt_a = SubElement(filedesc, 'publicationStmt')
+    # empty paragraph, otherwise license?
+    publicationstmt_p = SubElement(publicationstmt_a, 'p')
 
+    # source description
+    sourcedesc = SubElement(filedesc, 'sourceDesc')
+    source_bibl = SubElement(sourcedesc, 'bibl')
     biblfull = SubElement(sourcedesc, 'biblFull')
-    publicationstmt = SubElement(filedesc, 'publicationStmt')
-    # FRAGE subreddit unter publisher?
-    publication_date = SubElement(publicationstmt, 'date')
-    publication_date.text = docmeta['date']
-    publication_url = SubElement(publicationstmt, 'ptr', type='URL', target=docmeta['url'])
+    # title statement once again
+    bib_titlestmt2 = SubElement(biblfull, 'titleStmt')
+    bib_titlemain2 = SubElement(bib_titlestmt2, 'title', type='main')
+    bib_titlemain2.text = docmeta["title"]
 
+    # publication statement
+    publicationstmt = SubElement(biblfull, 'publicationStmt')
+    publisher = SubElement(publicationstmt, 'publisher')
+    publication_date = SubElement(publicationstmt, 'date')
+    publication_url = SubElement(publicationstmt, 'ptr', type='URL',
+                                 target=docmeta['url'])
+    publication_date.text = docmeta['date']
+
+    # profile description
+    if subreddit_loc == 'profiledesc':
+        profiledesc = SubElement(header, 'profileDesc')
+        textclass = SubElement(profiledesc, 'textClass')
+        subred = SubElement(textclass, 'subreddit')
+        term = SubElement(subred, 'term')
+        term.text = docmeta["subreddit"]
+
+    # text, body, comments
     text = SubElement(teidoc, "text")
     body = SubElement(text, "body")
     # correct level for responses? or rather under text or other?
-    responses = SubElement(text, "comments")
+    res = SubElement(body, "div", type="comments")
+    responses = SubElement(res, "list")
     # iterate comments to include responses
     for id, comment in comments.items():
-        if comment['body'] != '[deleted]' and comment['author'] != '[deleted]':
-            # FRAGE sollen subcomments von gelöschten comments trotzdem gelistet werden?
-            # auf reddit steht nur [gelöscht] und keine subcomments mehr
-            build_subcomments(responses, comment)
-                       
-    tei_str = tostring(teidoc, pretty_print=True, encoding='utf-8').decode('utf-8')
+        if not filtered:
+            if comment['body'] == '[deleted]' or \
+                comment['author'] == '[deleted]':
+                continue
+        build_subcomments(responses, comment)
+
+    tei_str = tostring(teidoc, pretty_print=True,
+                       encoding='utf-8').decode('utf-8')
     if output_dir:
-        ElementTree(teidoc).write(f'{output_dir}/{post_id}.xml', pretty_print=True, encoding='utf-8')
+        ElementTree(teidoc).write(f'{output_dir}/{post_id}.xml',
+                                  pretty_print=True, encoding='utf-8')
+    if validate:
+        # https://github.com/adbar/trafilatura/blob/5ce31d9f6c8d4854ce169678499326355c8d1cee/trafilatura/xml.py#L190
+        print(validate_tei(teidoc))
     return tei_str
 
-def demo():
-    print(json2xml('wohnen_trees/qqro38_tree.json'))
 
-def run(dir='wohnen_trees'):
+def demo():
+    """A short demo of the json2xml function."""
+    print(json2xml('wohnen_filtered_trees/qqro38_tree.json',
+             output_dir='wohnen_filtered_xml'))
+    print(json2xml('wohnen_filtered_trees/qlt4qy_tree.json',
+                   output_dir='wohnen_filtered_xml'))
+
+
+def run(dir='wohnen_filtered_trees'):
+    """Convert all json files in a directory to xml."""
     for file in os.listdir(dir)[:100]:
-        json2xml(f'{dir}/{file}')
+        json2xml(f'{dir}/{file}', output_dir='wohnen_filtered_xml')
+
 
 if __name__ == '__main__':
     demo()
+    # print(validate_tei(lxml.etree.parse('example2.xml')))
     # run()

b843d86ed2584862afd3985f79dfb080a70fc022
Adrien Barbaresi <barbaresi@bbaw.de>
2024-11-22 14:13:38 +0100

    clean up tests directory and add extraction test

---
+++ tests/extraction_tests.py
@@ -0,0 +1,18 @@
+import os
+
+
+from extractor.comment_tree import extract_comments
+
+
+TEST_DIR = os.path.abspath(os.path.dirname(__file__))
+TEST_FILE = os.path.join(TEST_DIR, "files/GermanRap_comments_small/GermanRap_comments_small.zst")
+
+
+def test_extraction():
+    comments = extract_comments(TEST_FILE)
+    assert len(comments) == 1028
+
+    first = comments[0]
+    assert first.get("author") == "zer0deathserryone"
+    assert first.get("permalink") == "/r/GermanRap/comments/176b4p3/ich_bin_coverartdesigner_und_möchte_ihnen_einige/k4kybh8/"
+    assert first.get("updated_on") == 1697128408
